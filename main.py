fbf541125ef03e62b896b748a63b611bda0c1725
DotVR46 <maggot8950@gmail.com>
2023-04-08 12:05:33 +0600

    Новые методы взаимодествия с аккаунтом, новая версия chromedriver

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ class InstagramBot:
         password_input.send_keys(Keys.ENTER)
         time.sleep(10)
 
+    # Лайкает фото по хештегу
     def like_photo_by_hashtag(self, hashtag):
         browser = self.browser
         browser.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
@@ -52,17 +53,13 @@ class InstagramBot:
 
         for url in posts_urls:
             try:
-                browser.get(url)
-                time.sleep(5)
-                like_button = browser.find_element(
-                    By.XPATH, '//span/*[contains(@aria-label, "Нравится")]'
-                )
-                like_button.click()
+                self.put_exactly_like(url)
                 time.sleep(random.randrange(80, 100))
             except Exception as ex:
                 print(ex)
                 self.close_browser()
 
+    # Проверяет наличие xpath
     def xpath_exist(self, url):
         browser = self.browser
         try:
@@ -72,6 +69,7 @@ class InstagramBot:
             exist = False
         return exist
 
+    # Ставит лайк на конкретный пост
     def put_exactly_like(self, user_post):
         browser = self.browser
         browser.get(user_post)
@@ -85,63 +83,82 @@ class InstagramBot:
             time.sleep(4)
             like_button = '//span/*[contains(@aria-label, "Нравится")]'
             browser.find_element(By.XPATH, like_button).click()
-            time.sleep(4)
             print(f"Лайк на пост: {user_post} поставлен")
+
+        # self.close_browser()
+
+    # Собирает все посты с аккаунта в файл имя_аккаунта.txt
+    def get_all_posts_urls(self, user_acc):
+        browser = self.browser
+        browser.get(user_acc)
+        time.sleep(4)
+
+        wrong_user_acc = "//span[contains(., 'К сожалению, эта страница недоступна.')]"
+        if self.xpath_exist(wrong_user_acc):
+            print("Такого пользователя не существует")
+        else:
+            print("Пользователь найден, ставим лайки")
+            time.sleep(4)
+
+            posts_count = int(
+                browser.find_element(
+                    By.XPATH,
+                    "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[1]/span/span",
+                ).text
+            )
+            loops_count = int(posts_count / 12)
+            print(loops_count)
+
+            posts_urls = []
+            for i in range(0, loops_count):
+                hrefs = browser.find_elements(By.TAG_NAME, "a")
+                hrefs = [
+                    item.get_attribute("href")
+                    for item in hrefs
+                    if "/p/" in item.get_attribute("href")
+                ]
+                for href in hrefs:
+                    posts_urls.append(href)
+                browser.execute_script(
+                    "window.scrollTo(0, document.body.scrollHeight);"
+                )
+                time.sleep(random.randrange(3, 5))
+                print(f"Итерация #{i}")
+
+            file_name = user_acc.split("/")[-2]
+            posts_urls = list(set(posts_urls))
+
+            with open(f"{file_name}.txt", "a") as file:
+                for post_url in posts_urls:
+                    file.write(post_url + "\n")
+
+    # Ставит лайки всем постам на аккаунте
+    def put_many_likes(self, user_acc):
+        browser = self.browser
+        self.get_all_posts_urls(user_acc)
+        file_name = user_acc.split("/")[-2]
+        time.sleep(4)
+        browser.get(user_acc)
+        time.sleep(4)
+
+        with open(f"{file_name}.txt") as file:
+            urls_list = file.readlines()
+
+            for url in urls_list[0:6]:
+                try:
+                    self.put_exactly_like(url)
+                    time.sleep(5)
+                except Exception as ex:
+                    print(ex)
+                    self.close_browser()
+
         self.close_browser()
 
+    # Скачивает контент со страницы пользователя
+    def download_user_media(self, user_acc):
+        pass
 
 my_bot = InstagramBot(config.get("ACC_USERNAME"), config.get("ACC_PASSWORD"))
 my_bot.login()
-my_bot.put_exactly_like("https://www.instagram.com/p/CqaENeks21L/")
-
-# def hashtag_search(username, password, hashtag):
-#     browser = webdriver.Chrome(service=Service("chromedriver/chromedriver"))
-#     try:
-#         browser.get("https://www.instagram.com/")
-#         time.sleep(random.randrange(3, 5))
-#
-#         username_input = browser.find_element("name", "username")
-#         username_input.clear()
-#         username_input.send_keys(username)
-#         time.sleep(2)
-#         password_input = browser.find_element("name", "password")
-#         password_input.clear()
-#         password_input.send_keys(password)
-#         password_input.send_keys(Keys.ENTER)
-#         time.sleep(10)
-#
-#         try:
-#             browser.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
-#             time.sleep(5)
-#
-#             for i in range(1, 4):
-#                 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#                 # browser.implicitly_wait(random.randrange(3, 5))
-#                 time.sleep(random.randrange(3, 5))
-#             hrefs = browser.find_elements(By.TAG_NAME, "a")
-#             posts_urls = [
-#                 item.get_attribute("href")
-#                 for item in hrefs
-#                 if "/p/" in item.get_attribute("href")
-#             ]
-#
-#             for url in posts_urls:
-#                 try:
-#                     browser.get(url)
-#                     time.sleep(5)
-#                     browser.implicitly_wait(15)
-#                     like_button = browser.find_element(By.XPATH, '//span/*[contains(@aria-label, "Нравится")]').click()
-#                     time.sleep(random.randrange(80, 100))
-#                 except Exception as ex:
-#                     print(ex)
-#         except Exception as ex:
-#             print(ex)
-#     except Exception as ex:
-#         print(ex)
-#     finally:
-#         browser.close()
-#         browser.quit()
-
-
-# login(config.get("ACC_USERNAME"), config.get("ACC_PASSWORD"))
-# hashtag_search(config.get("ACC_USERNAME"), config.get("ACC_PASSWORD"), "росписьнаткани")
+# my_bot.put_exactly_like("https://www.instagram.com/p/CqaENeks21L/")
+my_bot.put_many_likes("https://www.instagram.com/tort.bar/")
